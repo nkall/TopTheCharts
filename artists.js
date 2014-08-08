32dd0dab4465f4a7fa2f421ab63af141c864836b
nkall <nkallhovde@gmail.com>
2014-08-07 17:43:08 -0700

    Added singles quiz, various small tweaks

--- artists.js
+++ artists.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var chosenSong;
     var json;
     var questionCounter;
+    var answer;
     var jsonFilename = "";
 
     $(".genre").click(function(event){
@@ -30,29 +31,29 @@ $(document).ready(function(){
             jsonFilename = newJsonFilename;
             $.getJSON(jsonFilename, function(injson){
                 json = injson;
-                questionSetup();
-                chosenSong = getSong(json);
+                chosenSong = questionSetup(json);
+                answer = chosenSong["artist"];
             });
         } else {
-            questionSetup();
-            chosenSong = getSong(json);
+            chosenSong = questionSetup(json);
+            answer = chosenSong["artist"];
         }
         $(".genre").hide();
     });
 
     $("#submitButton").click(function(){
-        showResult($("#sliderBar").slider("value"), chosenSong, json);
+        var checkedArtist = $("#a" + $('input[name = "artist"]:radio:checked').val()).text();
+        showResult(checkedArtist, answer, chosenSong, json);
     });
 
     $("#nextSong").click(function(){
        if ($("#questionNo").text() === $("#questionMax").text()){
-           showFinalScore();
+            showFinalScore();
        } else {
-           questionSetup();
-           chosenSong = getSong(json);
-           questionCounter++;
-
-           $("#questionNo").text(questionCounter);
+            chosenSong = questionSetup(json);
+            answer = chosenSong["artist"];
+            questionCounter++;
+            $("#questionNo").text(questionCounter);
        }
     });
 
@@ -62,93 +63,97 @@ $(document).ready(function(){
 });
 
 function startSetup(){
-    $("#maintitle").text("Top the Charts");
+    $("#maintitle").text("Top the Charts - Artists Edition");
     $("#subtitle").text("Make sure to turn up your sound!");
     $("#questionNo").text("0");
     $("#score").text("0");
+    $("#rank").empty();
     $("#hiscore").text(getHighScore());
     $("#submitButton").hide();
     $("#nextSong").hide();
     $("#replayButton").hide();
-    $("#chosenYear").empty();
     $("#songInfo").empty();
     $(".genre").show();
+    $(".artistBubble").hide();
     $("#songEmbed").hide();
-    var slider = $("#sliderBar").slider({
-        value:1940,
-        min:1940,
-        max:2014,
-        step:1,
-        slide: function (event, ui){
-            $("#chosenYear").html("<strong>My Guess:</strong> " + ui.value);
-        }
-    });
-    $("#sliderBar").hide();
 }
 
-function questionSetup(){
-    $("#chosenYear").empty();
+function questionSetup(json){
     $("#songInfo").empty();
-    $("#sliderBar").show();
     $("#submitButton").show();
     $("#nextSong").hide();
-    $("#maintitle").text("When was it a hit?");
-    $("#subtitle").text("Make a guess with the slider below:");
-    $("#chosenYear").html("<strong>My Guess:</strong> " + $("#sliderBar").slider("value"));
+    $("#maintitle").text("Name the Artist");
+    $("#subtitle").text("Choose one of the options below:");
+    $(".artistBubble").show();
+    var songTuple = getSong(json);
+    embedYoutube(songTuple[0], false);
+    populateChoices(songTuple[1]["artist"], songTuple[1]["year"], json);
+    return songTuple[1];
 }
 
-function showResult(guess, song, json){
-    $("#sliderBar").hide();
+function populateChoices(answer, year, json){
+    var repeats = [];
+    var artistVariance = [0, 1, 2, 12, 75];
+    var isUsedPlace = new Array(artistVariance.length);
+    for (var i = 0; i < isUsedPlace.length; i++) { isUsedPlace[i] = false; }
+    var correctChoicePlace = Math.floor(Math.random() * (isUsedPlace.length + 1));
+    $("#a" + String(correctChoicePlace)).html(answer + "<br />");
+    isUsedPlace[correctChoicePlace] = true;
+    repeats.push(answer);
+    for (var i = 0; i < artistVariance.length; ++i){
+        var currChoice = getArtistChoice(year, artistVariance[i], repeats, json);
+        var currChoicePlace = Math.floor(Math.random() * (artistVariance.length - i));
+        while (isUsedPlace[currChoicePlace] === true){
+            currChoicePlace++;
+            if (currChoicePlace > isUsedPlace.length) { currChoicePlace = 0; }
+        }
+        $("#a" + String(currChoicePlace)).html(currChoice + "<br />");
+        isUsedPlace[currChoicePlace] = true;
+        repeats.push(currChoice);
+    }
+}
+
+function getArtistChoice(year, range, repeats, json){
+    var possChoices = [];
+    for (var i = 0; i < json["song"].length; i++) {
+        if (Math.abs(year - json["song"][i]["year"]) <= range){
+            possChoices.push(json["song"][i]);
+        }
+    }
+    var artistChoice;
+    var isRepeat = false;
+    while (artistChoice === undefined || isRepeat === true){
+        isRepeat = false;
+        var artistChoice = possChoices[(Math.floor(Math.random() * 
+                             possChoices.length))]["artist"];
+        for (var i = 0; i < repeats.length; ++i){
+            if (artistChoice === repeats[i]){ isRepeat = true; }
+        }
+    }
+    return artistChoice;
+}
+
+function showResult(guess, answer, song, json){
     $("#submitButton").hide();
-    $("#chosenYear").html("<strong>Your Guess:</strong> " + guess);
+    $(".artistBubble").hide();
+    $(".artistChoice").text("");
     $("#songInfo").text(song["artist"] + " - " + song["title"] +
                           " (" + song["year"] + ")");
-    var ptsEarned = 0;
-    switch (Math.abs(guess - song["year"])){
-        case 0:
-            ptsEarned = 10;
-            $("#maintitle").text("Spot On!");
-            $("#subtitle").text("+10 points");
-            break;
-        case 1:
-            ptsEarned = 7;
-            $("#maintitle").text("Excellent");
-            $("#subtitle").text("+7 points");
-            break;
-        case 2:
-            ptsEarned = 4;
-            $("#maintitle").text("Great Job");
-            $("#subtitle").text("+4 points");
-            break;
-        case 3:
-            ptsEarned = 3;
-            $("#maintitle").text("Well Done");
-            $("#subtitle").text("+3 points");
-            break;
-        case 4:
-            ptsEarned = 2;
-            $("#maintitle").text("Decent");
-            $("#subtitle").text("+2 points");
-            break;
-        case 5: //Intentional fallthrough
-        case 6:
-            ptsEarned = 1;
-            $("#maintitle").text("Not Bad");
-            $("#subtitle").text("+1 point");
-            break;
-        default:
-            $("#maintitle").text("No Dice");
-            $("#subtitle").text("+0 points. Ouch!");
-            break;
-   }
-   var currScore = parseInt($("#score").text());
-   $("#score").text(String(currScore + ptsEarned));
-   if (parseInt($("#score").text()) > parseInt($("#hiscore").text())){
-       $("#hiscore").text(String(currScore + ptsEarned));
-       setHighScore(currScore + ptsEarned);
-   }
-   embedYoutube("http://www.youtube.com/embed/" + song["vid"], true); 
-   $("#nextSong").show();
+    if (guess === answer){
+        $("#maintitle").text("Great Job");
+        $("#subtitle").text("+1 point");
+        var currScore = parseInt($("#score").text());
+        $("#score").text(String(currScore + 1));
+        if (parseInt($("#score").text()) > parseInt($("#hiscore").text())){
+            $("#hiscore").text(String(currScore + 1));
+            setHighScore(currScore + 1);
+        }
+    } else {
+        $("#maintitle").text("No Dice");
+        $("#subtitle").text("+0 points");
+    }
+    embedYoutube("http://www.youtube.com/embed/" + song["vid"], true); 
+    $("#nextSong").show();
 }
 
 function getSong(json){
@@ -158,8 +163,8 @@ function getSong(json){
                       parseInt(songTime[1], 10)) * 0.38);
     var fullUrl = "http://www.youtube.com/embed/" + chosenSong["vid"] +
                     "?autoplay=1" + "&start=" + startTime + "&end=" + (startTime + 40);
-    embedYoutube(fullUrl, false);
-    return chosenSong;
+    var urlSongTuple = [fullUrl, chosenSong];
+    return urlSongTuple;
 }
 
 function embedYoutube(songUrl, isVisible) {
@@ -185,15 +190,15 @@ function showFinalScore() {
     $("#nextSong").hide();
     $("#maintitle").text("Your Final Score:");
     endScore = $("#score").text();
-    possScore = String(parseInt($("#questionMax").text()) * 10);
-    $("#subtitle").text(endScore + " out of a possible " + possScore);
+    possScore = String(parseInt($("#questionMax").text()));
+    $("#subtitle").text(endScore + " out of " + possScore);
     $("#replayButton").show();
-    var rank = parseInt(possScore) - parseInt(endScore);
-    if (rank > 100 || rank < 0) { rank = 0; }
+    var rank = parseInt(possScore) - parseInt(endScore) + 1;
+    if (rank > 10 || rank < 0) { rank = 1; }
     $.getJSON("results.json", function(results){
         var resultSong = results["result"][rank];
         embedYoutube("http://www.youtube.com/embed/" + resultSong["vid"], true);
-        $("#chosenYear").html("<strong>Rank: " + resultSong["artist"] + "</strong>");
+        $("#rank").html("<strong>Rank: " + resultSong["artist"] + "</strong>");
         var billboardLink = "http://www.billboard.com/articles/columns/chart-beat/" +
                               "5557800/hot-100-55th-anniversary-by-the-numbers-top" +
                               "-100-artists-most-no";
@@ -207,14 +212,14 @@ function setHighScore(hiscore){
     var expireLen = 2592000000; //30 days
     var expireDate = new Date();
     expireDate.setTime(expireDate.getTime() + expireLen);
-    document.cookie = "hs=" + String(hiscore) + "; expires=" + expireDate.toGMTString();
+    document.cookie = "hsa=" + String(hiscore) + "; expires=" + expireDate.toGMTString();
 }
 
 function getHighScore(){
-    var scoreStart = document.cookie.indexOf("hs=") + 3;
+    var scoreStart = document.cookie.indexOf("hsa=") + 4;
     var scoreEnd = document.cookie.indexOf(";", scoreStart);
-    if (scoreEnd === -1) {scoreEnd = document.cookie.length;}
-    if (document.cookie.length === 0 || scoreStart === 2){ return 0; }
+    if (scoreEnd === -1) { scoreEnd = document.cookie.length; }
+    if (document.cookie.length === 0 || scoreStart === 3){ return 0; }
     var hiscore = parseInt(document.cookie.substring(scoreStart, scoreEnd));
     setHighScore(hiscore); //Refresh expiry date
     return hiscore;
